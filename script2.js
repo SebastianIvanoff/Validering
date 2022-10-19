ff1d7bf71d8faec00e86d1398bfb858867bf65ba
Sebastian Ivanoff <sebbe.ivanoff@gmail.com>
2022-10-19 23:46:36 +0200

    progress

--- script2.js
+++ script2.js
@@ -1,95 +1,42 @@
-const firstName = document.querySelector("firstName");
-const lastName = document.getElementById("lastName");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-const repeatPassword = document.getElementById("repeatPassword");
-const form = document.getElementById("validationForm");
-const errorElement = document.getElementById("errorMessage");
+const form = document.querySelector("#validationForm");
+const firstName = document.querySelector('#firstName');
+const lastName = document.querySelector('#lastName');
 
 
-form.addEventListener("submit", (e) => {
-e.preventDefault()
-
-    validateInputs();
-
-});
-
-const setError =(element, message) => {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.errorMessage')
-
-    errorDisplay.innerText = message;
-    inputControl.classList.add('error')
-    inputControl.classList.remove('succsess')
-}
-
-const setSuccess =(element, message) => {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.errorMessage')
-
-    errorDisplay.innerText = '';
-    inputControl.classList.add('succsess')
-    inputControl.classList.remove('error')
-}
-const regEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,6}$/;
+ const validateNames = (input) => {
+    if(input.value.trim() === ''){
+        console.log('Du måste skriva in ett namn')
+        input.classList.add('error')
+        input.classList.remove('success')
+        return false
+    }else if(input.value.length < 2){
+        console.log('namnet måste vara minst 2 tecken')
+        input.classList.add('error')
+        input.classList.remove('success')
+        return false;
+      } 
+      input.classList.add('success')
+  input.classList.remove('error')
+  return true
+ }
+ 
 
 
-const validateInputs = () => {
-    const firstNameValue = firstName.value.trim()
-    const lastNameValue = lastName.value.trim()
-    const emailValue = email.value.trim()
-    const passwordValue = password.value.trim()
-    const repeatPasswordValue = repeatPassword.value.trim()
 
-    if(firstNameValue === '') {
-        setError(firstName, 'First Name is required')
-        console.log(firstName, 'First Name is required')
-    }else{
-        setSuccess(firstName)
-    }
+ form.addEventListener('submit', (e) => {
+    e.preventDefault()
 
-    if(lastNameValue === '') {
-        setError(lastName, 'Last Name is required')
-        console.log(lastName, 'Last Name is required')
-    }else{
-        setSuccess(lastName)
-    }
+    validateNames(firstName)
+    validateNames(lastName)
 
-    if(emailValue === '') {
-        setError(email, 'Email is required')
-        console.log(email, 'Email is required')
-    }else if(!regEx.test(emailValue)){
-        setError(email, 'provide a valid Email')
-    }
-    else{
-        setSuccess(email)
-    }
 
-    if(passwordValue === ''){
-        setError(password, 'Password is required')
-        console.log(password, 'Password is required')
-    } else if(passwordValue.length < 6){
-        setError(password, 'Password is too short')
-    } else {
-        setSuccess(password)
-    }
+    if(validateNames(firstName) && validateNames(lastName)) {
+        console.log('grattis du vet vad du heter');
+        const user = {
+          firstName: firstName.value.trim(),
+          lastName: lastName.value.trim()
+        }
+        console.log(user)
+      }
 
-    if(repeatPasswordValue === ''){
-        setError(repeatPassword, 'Repeat Password')
-        console.log(repeatPassword, 'Repeat Password')
-    } else if(repeatPasswordValue !== passwordValue){
-        setError(repeatPassword, 'Password does not match')
-    } else {
-        setSuccess(repeatPassword)
-    }
-}
-
-// const user = {
-//   firstName: firstNameValue,
-//   lastName: lastNameValue,
-//   email: emailValue,
-//   password: passwordValue,
-//   repeatPassword: repeatPasswordValue
-// };
-
-// console.log(user)
+ })
