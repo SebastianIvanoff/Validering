6ac6023ee263bb64963bf3e018da03bd22fe3256
waivan <31401897+waivan@users.noreply.github.com>
2022-10-20 12:31:52 +0200

    Even more progress

--- script.js
+++ script.js
@@ -1,165 +1,161 @@
 const form = document.querySelector("#validationForm");
-const firstName = document.querySelector('firstName');
-const lastName = document.querySelector('lastName');
-const email = document.querySelector('email');
-const password = document.querySelector('password');
-const repeatPassword = document.querySelector('repeatPassword');
- const checkbox = document.querySelector('terms');
+const firstName = document.querySelector("#firstName");
+const lastName = document.querySelector("#lastName");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const repeatPassword = document.querySelector("#repeatPassword");
+const checkbox = document.querySelector("#terms");
+const errorMessage = document.querySelector("errorMessage");
 
-const validateFirstName = (input) => {
-  console.log(input)
-  
-
-  if (firstName.value.trim() === "" || firstName.value.length < 2) {
-    return setError(firstName);
-  } else {
-    setSuccess(firstName);
-    console.log("rätt namn");
+const validateNames = (input) => {
+  const regExName = /^[a-zA-ZåäöÅÄÖ ,.'-]+$/u;
+  if (input.value.trim() === "") {
+    console.log("Du måste skriva in ett namn");
+    input.classList.add("error");
+    input.classList.remove("success");
+    return false;
+  } else if (input.value.length < 2) {
+    console.log("namnet måste vara minst 2 tecken");
+    input.classList.add("error");
+    input.classList.remove("success");
+    return false;
+  } else if (!regExName.test(input.value)) {
+    console.log("Kan inte använda siffror");
+    input.classList.add("error");
+    input.classList.remove("success");
   }
-};
-
-const validateLastName = (input) => {
-  console.log(input)
-  
-  if (lastName.value.trim() === "" || lastName.value.length < 2) {
-    return setError(lastName);
-  } else {
-    setSuccess(lastName);
-  }
-};
-
-const validateEmail = (input) => {
-  console.log(input)
-  
-  const regEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,6}$/;
-  if (email.value.trim() === "") {
-    return setError(email);
-  } else if (!regEx.test(email.value)) {
-    return setError(email);
-  } else {
-    return setSuccess(email);
-  }
-};
-
-const validatePassword = (input) => {
-  console.log(input)
-  
-  if (password.value.trim().length === 0) {
-    return setError(password);
-  } else if (password.value.trim() < 6) {
-    setError(password);
-  } else {
-    return setSuccess(password);
-  }
-};
-
-const validateRepeatedPW = (input) => {
-  console.log(input)
-  if (repeatPassword.value.trim().length === 0) {
-    return setError(repeatPassword);
-  } else if (repeatPassword !== password) {
-    setError(repeatPassword);
-  } else {
-    setSuccess(repeatPassword);
-  }
-};
-
-const validateCheck = (id) => {
- 
-  if (!checkbox.checked) {
-    return setError(checkbox);
-  } else {
-    return setSuccess(checkbox);
-  }
-};
-
-const setSuccess = (input) => {
-  input.classList.remove("is-invalid");
-  input.classList.add("is-valid");
+  input.classList.add("success");
+  input.classList.remove("error");
   return true;
 };
 
-const setError = (input) => {
-  input.classList.add("is-invalid");
-  input.classList.remove("is-valid");
-  input.focus();
-  return false;
+const validateEmail = (input) => {
+  const regExEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,6}$/;
+  if (email.value.trim() === "") {
+    console.log("Du måste skriva in en email");
+    input.classList.add("error");
+    input.classList.remove("success");
+    return false;
+  } else if (!regExEmail.test(input.value)) {
+    input.classList.add("error");
+    input.classList.remove("success");
+    return false;
+  }
+  input.classList.add("success");
+  input.classList.remove("error");
+  return true;
+};
+
+const validatePassword = (input) => {
+  if (input.value.trim() === "") {
+    console.log("Du måste skrva in ett lösenord");
+    input.classList.add("error");
+    input.classList.remove("success");
+    return false;
+  } else if (input.value.trim().length < 6) {
+    console.log("Ditt lösenord bör var minst 6 tecken");
+    input.classList.add("error");
+    input.classList.remove("success");
+    return false;
+  }
+  input.classList.add("success");
+  input.classList.remove("error");
+  return true;
+};
+
+const validateRepeatedPW = (input) => {
+  if (input.value.trim() === "") {
+    console.log("Du måste skriva in samma lösenord");
+    input.classList.add("error");
+    input.classList.remove("success");
+    return false;
+  } else if (input.value.trim() !== password.value.trim()) {
+    console.log("Ditt lösenord matchar inte");
+    input.classList.add("error");
+    input.classList.remove("success");
+    return false;
+  }
+  input.classList.add("success");
+  input.classList.remove("error");
+  return true;
+};
+
+const validateCheck = (input) => {
+  if (!checkbox.checked) {
+    console.log("Not Checked");
+    input.classList.add("error");
+    input.classList.remove("success");
+  } else {
+    console.log("Checked");
+    input.classList.add("success");
+    input.classList.remove("error");
+    return true;
+  }
 };
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const errors = [];
+  
+  for(let i = 0; i < form.length; i++){
+    const inputId = '#' + form[i].id
 
-  for (let i = 0; i < form.length; i++) {
-    
- 
-    
-    const inputId = "#" + form[i].id;
-
-    if (form[i].type === "firstName") {
-      errors[i] = validateFirstName(inputId);
-    } else if (form[i].type === "lastName") {
-      errors[i] = validateLastName(inputId);
-    } else if (form[i].type === "email") {
-      errors[i] = validateEmail(inputId);
-    } else if (form[i].type === "password") {
-      errors[i] = validatePassword(inputId);
-    } else if (form[i].type === "repeatPassword") {
-      errors[i] = validateFirstName(inputId);
-    } else if (form[i].type === "checkbox") {
-      errors[i] = validateCheck(inputId);
+    if(form[i].type === 'text'){
+      errors[i] = validateNames(inputId)
     }
+    else if (form[i].type === 'email'){
+      errors[i] = validateEmail(inputId)
+    }
+    else if (form[i].type === 'password'){
+      errors[i] = validatePassword(inputId)
+    }
+
+    else if (form[i].type === 'repeatPassword'){
+      errors[i] = validatePassword(inputId)
+    }
+
+    else if (form[i].type === 'checkbox'){
+      errors[i] = validateCheck(inputId)
+    }
+
   }
 
   console.log(errors);
 
   if (errors.includes(false)) {
     console.log("Vi har fel i formuläret!");
+    return
   } else {
     console.log("allt är bra");
   }
 });
 
-const user = {
-  FirstName: firstName.value,
-  LastName: lastName.value,
-  Email: email.value,
-  Password: password.value,
-  RepeatPassword: repeatPassword.value,
-};
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
 
-console.log(user);
+//   validateNames(firstName);
+//   validateNames(lastName);
+//   validateEmail(email);
+//   validatePassword(password);
+//   validateRepeatedPW(repeatPassword);
+//   validateCheck(checkbox);
 
-// let i = 0;
-// for (input of e.target) {
-//   const inputID = "#" + form[i].id;
-//   switch (inputID.type) {
-//     case "FirstName":
-//       errors[i] = validateFirstName(inputID);
-//       break;
-
-//     case "LastName":
-//       errors[i] = validateLastName(inputID);
-//       break;
-
-//     case "email":
-//       errors[i] = validateEmail(inputID);
-//       break;
-
-//     case "password":
-//       errors[i] = validatePassword(inputID);
-//       break;
-
-//     case "repeatPassword":
-//       errors[i] = validateRepeatedPW(inputID);
-//       break;
-
-//     case "checkbox":
-//       errors[i] = validatePassword(inputID);
-
-//     default:
-//       break;
+//   if (
+//     validateNames(firstName) &&
+//     validateNames(lastName) &&
+//     validateEmail(email) &&
+//     validatePassword(password) &&
+//     validateRepeatedPW(repeatPassword) &&
+//     validateCheck(checkbox)
+//   ) {
+//     const user = {
+//       firstName: firstName.value.trim(),
+//       lastName: lastName.value.trim(),
+//       email: email.value.trim(),
+//       password: password.value.trim(),
+//       repeatPassword: repeatPassword.value.trim(),
+//     };
+//     console.log(user);
 //   }
-//   i++;
-// }
+// });
